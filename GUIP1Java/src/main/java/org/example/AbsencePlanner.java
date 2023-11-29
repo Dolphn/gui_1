da7798aa239b950953c962c9c109ba55bd39e179
Cornelius Engel <cornelius_johannes.engel@smail.th-koeln.de>
2023-11-29 13:24:50 +0100

    Grundbaustein der GUI / einbettung in Logik

--- GUIP1Java/src/main/java/org/example/AbsencePlanner.java
+++ GUIP1Java/src/main/java/org/example/AbsencePlanner.java
@@ -1,8 +1,14 @@
 package org.example;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 import java.sql.*;
 
-public class AbsencePlanner {
+public class AbsencePlanner extends Application {
     private Team team;
     private Connection connection;
 
@@ -12,21 +18,88 @@ public class AbsencePlanner {
         initializeDatabase();
     }
 
-    private void initializeDatabase() {
-        String createEmployeesTableSQL = "CREATE TABLE IF NOT EXISTS employees (" +
-                "id INTEGER PRIMARY KEY AUTOINCREMENT," +
-                "first_name TEXT NOT NULL," +
-                "last_name TEXT NOT NULL," +
-                "favorite_color TEXT NOT NULL);";
+    @Override
+    public void start(Stage stage) throws Exception {
+        //GUI erzeugen
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("main_window.fxml"));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.setTitle("Abwesenheitsplaner");
+            stage.show();
+        } catch (Exception e){
+            e.printStackTrace();
 
-        String createAbsencesTableSQL = "CREATE TABLE IF NOT EXISTS absences (" +
-                "id INTEGER PRIMARY KEY AUTOINCREMENT," +
-                "employee_id INTEGER," +
-                "type TEXT NOT NULL," +
-                "start_date TEXT NOT NULL," +
-                "end_date TEXT NOT NULL," +
-                "approved INTEGER NOT NULL," +
-                "FOREIGN KEY (employee_id) REFERENCES employees(id));";
+        }
+        //
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+        //Test
+        //Testgit
+        AbsencePlanner planner = new AbsencePlanner();
+
+        Connection con = SQLiteConnection.connect();
+
+        // Mitarbeiter hinzufügen
+        planner.addEmployee("Lisa", "Mustermann", "#87CEFA");
+
+        // Abwesenheitsantrag stellen
+        planner.requestAbsence("Lisa Mustermann", AbsenceType.VACATION, "2023-01-01", "2023-01-07");
+
+        // Abwesenheit löschen
+        planner.deleteAbsence("Lisa Mustermann", 0);
+
+        String query = "SELECT * FROM employees;";
+        try (Statement statement = con.createStatement();
+             ResultSet resultSet = statement.executeQuery(query)) {
+
+            // Mitarbeiterdaten aus der Abfrage auslesen und ausgeben
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String firstName = resultSet.getString("first_name");
+                String lastName = resultSet.getString("last_name");
+                String favoriteColor = resultSet.getString("favorite_color");
+
+                System.out.println("ID: " + id + ", Name: " + firstName + " " + lastName + ", Lieblingsfarbe: " + favoriteColor);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            // Verbindung schließen
+            try {
+                if (con != null) {
+                    con.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // Datenbankverbindung schließen
+        SQLiteConnection.disconnect(con);
+    }
+    private void initializeDatabase() {
+        String createEmployeesTableSQL = """
+                CREATE TABLE IF NOT EXISTS employees (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                first_name TEXT NOT NULL,
+                last_name TEXT NOT NULL,
+                favorite_color TEXT NOT NULL);
+                """;
+
+        String createAbsencesTableSQL = """
+                CREATE TABLE IF NOT EXISTS absences (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                employee_id INTEGER,
+                type TEXT NOT NULL,
+                start_date TEXT NOT NULL,
+                end_date TEXT NOT NULL,
+                approved INTEGER NOT NULL,
+                FOREIGN KEY (employee_id) REFERENCES employees(id));
+                """;
 
         try (PreparedStatement preparedStatement1 = connection.prepareStatement(createEmployeesTableSQL);
              PreparedStatement preparedStatement2 = connection.prepareStatement(createAbsencesTableSQL)) {
@@ -98,49 +171,4 @@ public class AbsencePlanner {
         return null;
     }
 
-    public static void main(String[] args) {
-        //Test
-        //Testgit
-        AbsencePlanner planner = new AbsencePlanner();
-
-        Connection con = SQLiteConnection.connect();
-
-        // Mitarbeiter hinzufügen
-        planner.addEmployee("Lisa", "Mustermann", "#87CEFA");
-
-        // Abwesenheitsantrag stellen
-        planner.requestAbsence("Lisa Mustermann", AbsenceType.VACATION, "2023-01-01", "2023-01-07");
-
-        // Abwesenheit löschen
-        planner.deleteAbsence("Lisa Mustermann", 0);
-
-        String query = "SELECT * FROM employees;";
-        try (Statement statement = con.createStatement();
-             ResultSet resultSet = statement.executeQuery(query)) {
-
-            // Mitarbeiterdaten aus der Abfrage auslesen und ausgeben
-            while (resultSet.next()) {
-                int id = resultSet.getInt("id");
-                String firstName = resultSet.getString("first_name");
-                String lastName = resultSet.getString("last_name");
-                String favoriteColor = resultSet.getString("favorite_color");
-
-                System.out.println("ID: " + id + ", Name: " + firstName + " " + lastName + ", Lieblingsfarbe: " + favoriteColor);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            // Verbindung schließen
-            try {
-                if (con != null) {
-                    con.close();
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
-
-        // Datenbankverbindung schließen
-        SQLiteConnection.disconnect(con);
-    }
 }
---
+++ GUIP1Java/src/main/java/org/example/Controller.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class Controller {
+    @FXML
+    public void employeesSorted(ActionEvent e){
+
+    }
+    public void teamsSorted(ActionEvent e){
+
+    }
+}
